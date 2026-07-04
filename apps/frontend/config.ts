6748548db129156a5c23440c6ef00094c21d805d
nitingupta95 <ng61315@gmail.com>
2026-07-04 13:28:02 +0530

    chore: update WS_URl to use environment variable with fallback default

--- apps/frontend/config.ts
+++ apps/frontend/config.ts
@@ -1,3 +1,3 @@
 export const HTTP_BACKEND = process.env.NEXT_PUBLIC_HTTP_BACKEND || "/api";
 
-export const WS_URl= "ws://localhost:8088"
+export const WS_URl = process.env.NEXT_PUBLIC_WS_URL || "ws://localhost:8088";
